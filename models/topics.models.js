77708be1ad5566072e91150cea42e2bed1473923
Toby Chan <toby70@hotmail.co.uk>
2024-02-19 13:00:41 +0000

    finished the first endpoint task with testing and error handling

---
+++ models/topics.models.js
@@ -0,0 +1,9 @@
+const db = require("../db/connection")
+
+function selectAllTopics() {
+  return db.query('SELECT * FROM topics;').then((result) => {
+    return result.rows;
+  })
+}
+
+module.exports = {selectAllTopics}
