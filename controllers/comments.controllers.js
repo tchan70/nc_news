71965ac66bf90445f1f7f4350b06fcae48aca2ce
Toby Chan <toby70@hotmail.co.uk>
2024-02-22 12:35:41 +0000

    removed console logs

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,11 +2,9 @@ const { selectCommentById, removeCommentById } = require("../models/comments.mod
 
 function deleteCommentById(req, res, next){
   const{comment_id} = req.params
-  console.log("i got here 1")
   const promises = [selectCommentById(comment_id), removeCommentById(comment_id)]
   Promise.all(promises)
   .then(() =>{
-    console.log("i got here 2")
     res.status(204).send()
   })
   .catch(next)
